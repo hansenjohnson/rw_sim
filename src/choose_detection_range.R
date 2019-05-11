bb84b72572410b3690eaf4d6bb752e185fa7fbac
Hansen Johnson <hansen.johnson@dal.ca>
2019-05-11 11:50:12 -0300

    update detection range params

--- src/choose_detection_range.R
+++ src/choose_detection_range.R
@@ -1,10 +1,12 @@
 ## choose_detection_range ##
 
+source('src/functions.R')
+
 # short
 nrws = 1e4
 L = 1
-x0 = 5
-k = -0.7
+x0 = 6
+k = -0.5
 
 ini = init_acoustic(nrws = nrws, L = L, x0 = x0, k = k)
 plot_init_acoustic(ini = ini, L = L, x0 = x0, k = k, fig_dir = 'tmp/')
@@ -13,13 +15,13 @@ plot_init_acoustic(ini = ini, L = L, x0 = x0, k = k, fig_dir = 'tmp/')
 nrws = 1e4
 L = 1
 x0 = 10
-k = -0.6
+k = -0.3
 
 ini = init_acoustic(nrws = nrws, L = L, x0 = x0, k = k)
 plot_init_acoustic(ini = ini, L = L, x0 = x0, k = k, fig_dir = 'tmp/')
 
 # long
-nrws = 1e5
+nrws = 1e4
 L = 1
 x0 = 20
 k = -0.3
